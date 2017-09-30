27b33d8b532bb3157eb3a10e479274500f194905
mtali <mtali.emmanuel@protonmail.com>
2017-09-30 12:09:49 +0300

    Add basic main fragmed code and styles including basic search functionality

---
+++ app/src/main/java/com/example/goodluck/a4u/ux/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.goodluck.a4u.ux;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.goodluck.a4u.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static MainActivity mInstance = null;
+
+    /**
+     * Returns Main activity instance. Null if the activity doesn't exist
+     *
+     * @return activity instance
+     */
+    private static synchronized MainActivity getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mInstance = this;
+
+        setContentView(R.layout.activity_main);
+
+        // Prepare tool bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+        } else {
+            Log.e(TAG, "GetSupportActionBar returned null");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Prepare search view
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        if (null != searchItem) {
+            prepareSearchView(searchItem);
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void prepareSearchView(final MenuItem searchItem) {
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setSubmitButtonEnabled(true);
+        SearchManager searchManager = (SearchManager) MainActivity.this.getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(MainActivity.this.getComponentName()));
+        SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Submit search query and hide search action view
+                onSearchSubmitted(query);
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                searchItem.collapseActionView();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        };
+        searchView.setOnQueryTextListener(queryTextListener);
+
+    }
+
+    /**
+     * Method create new category fragment with defined search query
+     * @param searchQuery text used for products search
+     */
+    private void onSearchSubmitted(String searchQuery) {
+
+    }
+}
