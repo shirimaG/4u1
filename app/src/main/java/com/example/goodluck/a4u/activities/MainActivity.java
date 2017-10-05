347023e4ad0080ee173f19dd2c5e3ce0bbe016ac
mtali <mtali.emmanuel@protonmail.com>
2017-10-05 12:49:19 +0300

    implement fragment management methods and call back to product click!

--- app/src/main/java/com/example/goodluck/a4u/activities/MainActivity.java
+++ app/src/main/java/com/example/goodluck/a4u/activities/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.goodluck.a4u.activities;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
@@ -10,10 +13,16 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.goodluck.a4u.BuildConfig;
 import com.example.goodluck.a4u.R;
+import com.example.goodluck.a4u.fragments.ProductsFeedFragment;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MSG_MAIN_ACTIVITY_IS_NULL = "MainActivity instance is null";
     private static final String TAG = MainActivity.class.getSimpleName();
     private static MainActivity mInstance = null;
 
@@ -26,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
         return mInstance;
     }
 
+    /**
+     * Update actionBar title
+     */
+
+    public static void setActionBarTitle(String title) {
+        MainActivity instance = MainActivity.getInstance();
+        if (null != instance) {
+            instance.setTitle(title);
+        } else {
+            Log.e(TAG, MSG_MAIN_ACTIVITY_IS_NULL);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Prepare tool bar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -42,6 +64,42 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.e(TAG, "GetSupportActionBar returned null");
         }
+
+        addInitialFragment();
+    }
+
+    private void addInitialFragment() {
+        clearBackStack();
+        Timber.d("Products feed fragment called");
+        Fragment fragment = ProductsFeedFragment.newInstance();
+        replaceFragment(fragment, ProductsFeedFragment.class.getSimpleName());
+    }
+
+    private void replaceFragment(Fragment newFragment, String transactionTag) {
+        if (newFragment != null) {
+            FragmentManager frgManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = frgManager.beginTransaction();
+            fragmentTransaction.addToBackStack(transactionTag);
+            fragmentTransaction.replace(R.id.main_content_frame, newFragment).commit();
+            frgManager.executePendingTransactions();
+        } else {
+            Timber.e(new RuntimeException(), "Replace fragments with null newFragment parameter");
+        }
+    }
+
+    private void clearBackStack() {
+        Timber.d("Clearing backstack");
+        FragmentManager manager = getSupportFragmentManager();
+        if (manager.getBackStackEntryCount() > 0) {
+            if (BuildConfig.DEBUG) {
+                for (int i = 0; i < manager.getBackStackEntryCount(); i++) {
+                    Timber.d("BackStack content_%d= %d, name: %s", i, manager.getBackStackEntryAt(i).getId(), manager.getBackStackEntryAt(i).getName());
+                }
+            }
+            FragmentManager.BackStackEntry first = manager.getBackStackEntryAt(0);
+            manager.popBackStackImmediate(first.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+        Timber.d("BackStack cleared");
     }
 
     @Override
@@ -85,9 +143,19 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Method create new category fragment with defined search query
+     *
      * @param searchQuery text used for products search
      */
     private void onSearchSubmitted(String searchQuery) {
-
+        Timber.d("Search submitted: ?search=%s",searchQuery);
     }
+
+    /**
+     * Launch ProductFragment
+     */
+
+    public void onProductSelected(long productId) {
+        Toast.makeText(this, "Product clicked " + String.valueOf(productId), Toast.LENGTH_SHORT).show();
+    }
+
 }
