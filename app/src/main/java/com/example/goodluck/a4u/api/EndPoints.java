467b439df66e5b6d0808fba99501c80fda249846
mtali <mtali.emmanuel@protonmail.com>
2017-10-05 12:51:11 +0300

    Construct API endpoint

---
+++ app/src/main/java/com/example/goodluck/a4u/api/EndPoints.java
@@ -0,0 +1,13 @@
+package com.example.goodluck.a4u.api;
+
+
+public class EndPoints {
+
+    /**
+     * Base server url
+     */
+    private static final String API_URL = "https://balongo.herokuapp.com";
+
+    public static final String PRODUCTS = API_URL.concat("/api/products/?format=json");
+
+}
